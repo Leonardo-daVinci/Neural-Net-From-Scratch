087da803a7bf2af0a1cf6b067a7976cbf0019fd9
Akshit Keoliya <akshitkeoliya4@gmail.com>
2021-11-08 18:17:18 -0800

    4 - Implementing CategoricalCrossEntropy Loss

--- LayerDense.py
+++ LayerDense.py
@@ -38,19 +38,68 @@ class ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = LayerDense(4, 5)
-# here output of layer 1 will be (x,5), so we need to have layer 2 of the shape (5,y)
-layer2 = LayerDense(5, 2)
-layer1.forward(X)
-# print(layer1.output)
-layer2.forward(layer1.output)
-# print(layer2.output)
+class Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        batch_loss = np.mean(sample_losses)
+        return batch_loss
+
+
+class CategoricalCrossEntropyLoss(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # handling type of class targets
+        if len(y_true.shape) == 1:
+            # scalar class values
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            # one-hot encoded class values
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+# layer1 = LayerDense(4, 5)
+# # here output of layer 1 will be (x,5), so we need to have layer 2 of the shape (5,y)
+# layer2 = LayerDense(5, 2)
+# layer1.forward(X)
+# # print(layer1.output)
+# layer2.forward(layer1.output)
+# # print(layer2.output)
 X, y = spiral_data(100, 3)
-layer1 = LayerDense(2, 5)
-activation1 = ReLU()
-layer1.forward(X)
-# print(layer1.output)
+# layer1 = LayerDense(2, 5)
+# activation1 = ReLU()
+# layer1.forward(X)
+# # print(layer1.output)
+#
+# activation1.forward(layer1.output)
+# print(activation1.output)
 
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1 = LayerDense(2, 3)
+activation1 = ReLU()
+
+dense2 = LayerDense(3, 3)
+activation2 = Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss_function = CategoricalCrossEntropyLoss()
+loss = loss_function.calculate(activation2.output, y)
+print("Loss", loss)
